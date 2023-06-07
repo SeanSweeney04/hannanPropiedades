e24ed7748a4f2739778fbd881d22601fb8f29db9
SeanSweeney04 <115557622+SeanSweeney04@users.noreply.github.com>
2023-06-07 13:58:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -133,100 +133,110 @@
 
 
 const express = require('express');
-const request = require('request');
-const async = require('async');
+const request = require('request-promise');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-const pageSize = 10; // Número de propiedades por página
+const API_TOKEN = 'a891875b348b4c68b615e8cb56abff72';
+const API_URL = 'https://witei.com/api/v1/houses/';
+const PAGE_SIZE = 10;
 
-app.get('/', (req, res) => {
-  const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
-  const propiedades_disponibles = [];
-  const page = parseInt(req.query.page) || 1; // Página solicitada
+app.get('/', async (req, res) => {
+  try {
+    const propiedades_disponibles = [];
+    const page = parseInt(req.query.page) || 1;
+    const totalPages = await getTotalPages();
 
-  async.timesSeries(5, (pageNumber, next) => {
-    const url = `https://witei.com/api/v1/houses/?page=${pageNumber+1}&status=available`;
-    request({url, headers}, (error, response, body) => {
-      if (error) {
-        next(error);
+    for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+      const url = `${API_URL}?page=${pageNumber}&status=available`;
+      const data = await getApiData(url);
+      
+      if (data.results) {
+        propiedades_disponibles.push(...data.results);
       } else {
-        const data = JSON.parse(body);
-        console.log(data); // Verificar el contenido de data
-        if (data.results) {
-          propiedades_disponibles.push(...data.results);
-        } else {
-          console.log('No se encontraron resultados en la página', pageNumber+1);
-        }
-        next();
+        console.log('No se encontraron resultados en la página', pageNumber);
       }
+    }
+
+    const startIndex = (page - 1) * PAGE_SIZE;
+    const endIndex = startIndex + PAGE_SIZE;
+    const propiedades_pagina = propiedades_disponibles.slice(startIndex, endIndex);
+
+    res.render('propiedades', {
+      propiedades: propiedades_pagina,
+      currentPage: page,
+      totalPages: totalPages
     });
-  }, (error) => {
-    if (error) {
-      res.status(500).send('Error interno del servidor');
-    } else {
-      const startIndex = (page - 1) * pageSize; // Índice de la primera propiedad de la página solicitada
-      const endIndex = startIndex + pageSize; // Índice de la última propiedad de la página solicitada
-      const propiedades_pagina = propiedades_disponibles.slice(startIndex, endIndex);
-      const totalPages = Math.ceil(propiedades_disponibles.length / pageSize); // Número total de páginas
-      res.render('propiedades', {
-        propiedades: propiedades_pagina,
-        currentPage: page,
-        totalPages: totalPages
-      });
-    }
-  });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error interno del servidor');
+  }
 });
 
-app.get('/propiedad/:prop_id', (req, res) => {
-  const prop_id = req.params.prop_id;
-  const url = `https://witei.com/api/v1/houses/?identifier=${prop_id}`;
-  const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
+app.get('/propiedad/:prop_id', async (req, res) => {
+  try {
+    const prop_id = req.params.prop_id;
+    const url = `${API_URL}?identifier=${prop_id}`;
+    const data = await getApiData(url);
 
-  request({url, headers}, (error, response, body) => {
-    if (error) {
-      res.status(500).send('Error interno del servidor');
+    if (data.count > 0 && data.results[0].identifier === prop_id) {
+      res.render('propiedad_detalle', { detalle: data.results });
     } else {
-      const data = JSON.parse(body);
-      if (data.count > 0 && data.results[0].identifier === prop_id) {
-        res.render('propiedad_detalle', {detalle: data.results});
-      } else {
-        res.status(404).send('Propiedad no encontrada o no disponible');
-      }
+      res.status(404).send('Propiedad no encontrada o no disponible');
     }
-  });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error interno del servidor');
+  }
 });
 
-app.get('/home', (req, res) => {
-  const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
+app.get('/home', async (req, res) => {
+  try {
+    const propiedades_disponibles = await getAvailableProperties();
+
+    res.render('home', { propiedades: propiedades_disponibles });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+
+async function getApiData(url) {
+  const options = {
+    url,
+    headers: {
+      'Authorization': `Bearer ${a891875b348b4c68b615e8cb56abff72}`
+    },
+    json: true
+  };
+
+  const response = await request(options);
+  return response;
+}
+
+async function getTotalPages() {
+  const url = `${API_URL}?status=available`;
+  const data = await getApiData(url);
+  const totalProperties = data.count;
+  const totalPages = Math.ceil(totalProperties / PAGE_SIZE);
+  return totalPages;
+}
+
+async function getAvailableProperties() {
   const propiedades_disponibles = [];
+  const totalPages = await getTotalPages();
 
-  async.timesSeries(5, (pageNumber, next) => {
-    const url = `https://witei.com/api/v1/houses/?page=${pageNumber+1}&status=available`;
-    request({url, headers}, (error, response, body) => {
-      if (error) {
-        next(error);
-      } else {
-        const data = JSON.parse(body);
-        if (data.results) {
-          propiedades_disponibles.push(...data.results);
-        } else {
-          console.log('No se encontraron resultados en la página', pageNumber+1);
-        }
-        next();
-      }
-    });
-  }, (error) => {
-    if (error) {
-      res.status(500).send('Error interno del servidor');
+  for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+    const url = `${API_URL}?page=${pageNumber}&status=available`;
+    const data = await getApiData(url);
+    
+    if (data.results) {
+      propiedades_disponibles.push(...data.results);
     } else {
-      res.render('home', { propiedades: propiedades_disponibles });
+      console.log('No se encontraron resultados en la página', pageNumber);
     }
-  });
-});
+  }
 
-app.listen(port, () => {
-  console.log('La aplicación está funcionando en el puerto '+ port);
-});
+  return propiedades
